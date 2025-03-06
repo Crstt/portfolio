95e71dd47861ed4d5da3efe1c388e6313f14b885
Crstt <matteo598.catalano@gmail.com>
2025-03-05 21:03:17 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return <div className="bg-blue-500 text-white p-6 text-xl">
+    Welcome to My Portfolio
+  </div>
+  ;
+  }
+   
